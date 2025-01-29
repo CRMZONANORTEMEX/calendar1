514e6f90f26a7fc6cfca19a1af07a747ced2b0b0
CRMZONANORTEMEX <enfermeriacruzrojazonanorte@gmail.com>
2025-01-28 22:45:00 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyq8K0sgAZEejmGwxHa14dLSkhvnzleqyTzwtaowOkZNOAoPU11hN_yVqPe9h_wp2Ac/exec";
